e6a51deefa56b84780dc36cd34b0da4dfb830152
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2022-10-22 16:37:49 +0800

    Decided to redo my code since i keep having the same error

--- namePrint_asterisk.py
+++ namePrint_asterisk.py
@@ -10,19 +10,19 @@ print("Hello, my nickname is...\n")
 
 #make a pattern JEL from Jelen
 str1 = "JEL"
-letter_J = [[" " for i in range(6)] for j in range(6)]
-letter_E = [[" " for i in range(6)] for j in range(6)]
-letter_L = [[" " for i in range(6)] for j in range(6)]
+letter_J = [["" for i in range(5)] for j in range(6)]
+letter_E = [["" for i in range(5)] for j in range(6)]
+letter_L = [["" for i in range(5)] for j in range(6)]
 
 #code for N
 for row in range(6):
-    for col in range(6):
-        if col==0 or col==6 or row==col:
+    for col in range(5):
+        if (col==0 or row==4) or (row==0 or col==2) or (row==5 and col==1):
             letter_J[row][col]= "*"
 
 #code for E
 for row in range(6):
-    for col in range(6):
+    for col in range(5):
         if col==0 or ((row==0 or row==3 or row==6) and (col>0)):
             letter_E[row][col]="*"
 
@@ -32,11 +32,11 @@ for row in range(6):
         if col==0 or row==6 and col>0:
             letter_L[row][col]="*"
 
-for row in range(6):
-    for col in range(6):
-        print(letter_J[row][col],end="")
-    for col in range(6):
-        print(letter_E[row][col],end="")
-    for col in range(6):
-        print(letter_L[row][col],end="")
+for i in range(6):
+    for j in range(5):
+        print(letter_J[i][j],end="")
+    for j in range(5):
+        print(letter_E[i][j],end="")
+    for j in range(5):
+        print(letter_L[i][j],end="")
 print()
