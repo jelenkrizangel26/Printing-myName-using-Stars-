bd6e3317f6eedeb751ceb0dbd8e44ab327e502cd
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2022-10-22 15:54:37 +0800

    Trying to figure out where is my error

--- namePrint_asterisk.py
+++ namePrint_asterisk.py
@@ -2,12 +2,41 @@
 #Create a program that will print your nickname using only asterisk character(*). 
 #Your program should be uploaded to github before 11:59 PM.
 
+
+
 #Introduction
 print("Hello, my nickname is...\n")
+
+
 #make a pattern JEL from Jelen
 str1 = "JEL"
-letter_J = [[" " for i in range(4)] for j in range(4)]
+letter_J = [[" " for i in range(6)] for j in range(6)]
+letter_E = [[" " for i in range(6)] for j in range(6)]
+letter_L = [[" " for i in range(6)] for j in range(6)]
 
-letter_E = [[" " for i in range(4)] for j in range(4)]
+#code for N
+for row in range(6):
+    for col in range(6):
+        if col==0 or col==6 or row==col:
+            letter_J[row][col]= "*"
 
-letter_L = [[" " for i in range(4)] for j in range(4)]
+#code for E
+for row in range(6):
+    for col in range(6):
+        if col==0 or ((row==0 or row==3 or row==6) and (col>0)):
+            letter_E[row][col]="*"
+
+#code for L
+for row in range(6):
+    for col in range(6):
+        if col==0 or row==6 and col>0:
+            letter_L[row][col]="*"
+
+for row in range(6):
+    for col in range(6):
+        print(letter_J[row][col],end="")
+    for col in range(6):
+        print(letter_E[row][col],end="")
+    for col in range(6):
+        print(letter_L[row][col],end="")
+print()
