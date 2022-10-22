f25224697ed5811bd61b25f476b4797009820775
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2022-10-22 13:33:15 +0800

    Done adding measures and intro

---
+++ namePrint_asterisk.py
@@ -0,0 +1,11 @@
+#ASSIGNMENT 1
+#Create a program that will print your nickname using only asterisk character(*). 
+#Your program should be uploaded to github before 11:59 PM.
+
+#First indicate the number of lines(height) and width of the pattern.
+height = 8
+width = (2 * height) - 1
+
+#Introduction
+
+print("Hello, my nickname is...")
