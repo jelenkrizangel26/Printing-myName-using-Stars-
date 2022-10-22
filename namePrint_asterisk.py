559deca8fa26fbb21b32422b2052ef94ae343a76
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2022-10-22 16:49:03 +0800

    I learned that I have error in spaces and forget to add a parenthesis in 1 part. Will be needing to edit the letter L

--- namePrint_asterisk.py
+++ namePrint_asterisk.py
@@ -9,34 +9,35 @@ print("Hello, my nickname is...\n")
 
 
 #make a pattern JEL from Jelen
-str1 = "JEL"
-letter_J = [["" for i in range(5)] for j in range(6)]
-letter_E = [["" for i in range(5)] for j in range(6)]
-letter_L = [["" for i in range(5)] for j in range(6)]
-
-#code for N
-for row in range(6):
-    for col in range(5):
-        if (col==0 or row==4) or (row==0 or col==2) or (row==5 and col==1):
-            letter_J[row][col]= "*"
-
-#code for E
-for row in range(6):
-    for col in range(5):
-        if col==0 or ((row==0 or row==3 or row==6) and (col>0)):
-            letter_E[row][col]="*"
+str1 = "JEL" 
+letter_J = [[" " for i in range(5)] for j in range(6)] 
+letter_E = [[" " for i in range(5)] for j in range(6)] 
+letter_L =[[" " for i in range(5)] for j in range(6)] 
+ 
+for row in range(6): 
+    for col in range(5): 
+        if (col==0 and row==4) or (row==0 or col==2) or (row==5 and col==1): 
+            letter_J[row][col]= "*" 
+             
+# code for E 
+for row in range(6): 
+    for col in range(5): 
+        if ((col==0 or row==5) or (col==0 or row==0) or (row==2 and (col!=3 and col!=4))): 
+            letter_E[row][col]= "*"  
 
 #code for L
 for row in range(6):
     for col in range(6):
-        if col==0 or row==6 and col>0:
-            letter_L[row][col]="*"
-
-for i in range(6):
+        if col==0 or (row==6 and col>0):
+            letter_L[row][col]= "*"
+             
+for i in range(6): 
+    print(end="  ") 
+    for j in range(5): 
+        print(letter_J[i][j], end=" ") 
+    print(end="  ") 
+    for j in range(5): 
+        print(letter_E[i][j], end=" ") 
     for j in range(5):
-        print(letter_J[i][j],end="")
-    for j in range(5):
-        print(letter_E[i][j],end="")
-    for j in range(5):
-        print(letter_L[i][j],end="")
-print()
+        print(letter_L[i][j], end=" ")
+    print()
